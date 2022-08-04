ccefe5041e5fb63a685ceb9340549c79872887fd
Azar Barfipour <azar.barfipour@gmail.com>
2022-08-04 14:46:13 -0700

    init

---
+++ client/MainRouter.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Route, Routes, Switch } from "react-router-dom";
+import Home from "./core/Home";
+import Users from "./user/Users";
+import Signup from "./user/Signup";
+import Signin from "./auth/Signin";
+import EditProfile from "./user/EditProfile";
+import Profile from "./user/Profile";
+import PrivateRoute from "./auth/PrivateRoute";
+import Menu from "./core/Menu";
+
+const MainRouter = () => {
+  return (
+    <div>
+      <Menu />
+      {/* <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/users" component={Users}/>
+        <Route path="/signup" component={Signup}/>
+        <Route path="/signin" component={Signin}/>
+        <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
+        <Route path="/user/:userId" component={Profile}/>
+      </Switch> */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <PrivateRoute path="/user/edit/:userId" element={<EditProfile />} />
+        <Route path="/user/:userId" element={<Profile />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default MainRouter;
